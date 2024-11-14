0a38d1c4490c00efcdfe84b19e1920f311084f15
kaschlottag <kaschlottag@gmail.com>
2024-11-13 22:20:18 -0300

    Add files via upload

---
+++ 1-meuPrimeiroPrograma.js
@@ -0,0 +1,13 @@
+//alert('Testando o Script');
+
+var nome = prompt('Qual seu nome? ');
+var sobrenome = prompt('Qual seu sobrenome?');
+
+console.log ('Ola ' + nome + ' ' + sobrenome);
+
+//saida no DOM;
+
+document.write("<h1>Bem vindo " + nome + " " + sobrenome + "</h1>");
+document.write("Steve pelo JavaScript")
+
+document.write("<img src=https://sujeitoprogramador.com/steve.png alt=Foto do Steve Jobs>")
